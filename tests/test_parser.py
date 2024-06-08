7c9fa5e6c930a451a8f945357863bdebffd7cc6f
hsh814 <hsh814@naver.com>
2024-06-08 15:10:28 +0900

    Add more test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,6 +10,9 @@ class TestParser(unittest.TestCase):
         parser = sbsv.parser()
         parser.add_schema("[mem] [neg] [id: str] [file: str]")
         test_str = "[mem] [neg] id is [id myid] and file is [file myfile!]\n"
+        test_str += "[mem] [neg] id is [id myid2] and file is [file [myfile2!]]\n"
         result = parser.loads(test_str)
         self.assertEqual(result["mem"]["neg"][0]["id"], "myid")
         self.assertEqual(result["mem"]["neg"][0]["file"], "myfile!")
+        self.assertEqual(result["mem"]["neg"][1]["id"], "myid2")
+        self.assertEqual(result["mem"]["neg"][1]["file"], "[myfile2!]")
